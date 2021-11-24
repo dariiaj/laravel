c060fd5071118300c85e7094424259135c090308
dariiaj <dariia.zhohlo@oa.edu.ua>
2021-11-24 21:20:56 +0300

    2lab

--- database/seeders/BlogCategoriesTableSeeder.php
+++ database/seeders/BlogCategoriesTableSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 
 class BlogCategoriesTableSeeder extends Seeder
 {
@@ -13,6 +15,25 @@ class BlogCategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = [];
+
+        $cName = 'Без категорії';
+        $categories[] = [
+            'title'     => $cName,
+            'slug'      => Str::slug($cName),
+            'parent_id' => 0,
+        ];
+
+        for ($i = 1; $i <=10; $i++) {
+            $cName = 'Категорія #'.$i;
+            $parentId = ($i > 4) ? rand(1, 4) : 1;
+
+            $categories[] = [
+                'title'     => $cName,
+                'slug'      => Str::slug($cName),
+                'parent_id' => $parentId,
+            ];
+        }
+        DB::table('blog_categories')->insert($categories);
     }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +15,18 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data = [
+            [
+                'name' => 'Невідомий автор',
+                'email' => 'author_unknown@g.g',
+                'password' => bcrypt(Str::random(16)), // password
+            ],
+            [
+                'name' => 'Автор',
+                'email' => 'author1@g.g',
+                'password' => bcrypt('123123'), // password 123123
+            ],
+        ];
+        DB::table('users')->insert($data);
     }
 }
